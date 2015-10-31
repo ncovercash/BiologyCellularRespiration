bc148a9f7e65d0bdfff54f3149beb2b35583c2da
noah <smileytechguy@smileytechguy.com>
2015-10-30 22:33:59 -0400

    Init files and libs

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<meta charset="utf-8" />
+		<title>Cole Lewis | Noah Overcash | Cellular Respiration Project</title>
+		<link rel="stylesheet" type="text/css" href="css.css">
+		<script src="js.js"></script>
+	</head>
+	<body>
+		test
+	</body>
+</html>
