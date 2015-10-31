8547bf964e071ef5769b346315afe5b6acc47e1f
noah <smileytechguy@smileytechguy.com>
2015-10-31 00:23:21 -0400

    First step of Glycolysis

--- index.php
+++ index.php
@@ -7,6 +7,73 @@
 		<script src="js.js"></script>
 	</head>
 	<body>
-		test
+		<ul id="menu">
+			<li data-menuanchor="Glycolysis" class="active"><a href="#Glycolysis">Glycolysis</a></li>
+			<li data-menuanchor="IntermediateStep"><a href="#IntermediateStep">Intermediate Step</a></li>
+			<li data-menuanchor="KrebsCycle"><a href="#KrebsCycle">Kreb's Cycle</a></li>
+			<li data-menuanchor="ETC"><a href="#ETC">Electron Transport Chain</a></li>
+		</ul>
+		<div id="project-main">
+			<div class="section glycolysis" id="section0">
+				<h1>Glycolysis</h1>
+				<table>
+					<thead>
+						<tr>
+							<th>
+								Inputs
+							</th>
+							<th>
+								Outputs
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td class="hidden GlucoseIn">
+								Glucose (C<sub>6</sub>H<sub>12</sub>O<sub>6</sub>)
+							</td>
+							<td class="hidden PyruvateOut">
+								2 Pyruvate (C<sub>3</sub>H<sub>3</sub>O<sub>3</sub>)
+							</td>
+						</tr>
+						<tr>
+							<td class="hidden ATPIn">
+								2 ATP
+							</td>
+							<td class="hidden ATPOut">
+								4 ATP (Net Gain: 2)
+							</td>
+						</tr>
+						<tr>
+							<td class="hidden NADPlusIn">
+								2 NAD<sup>+</sup>
+							</td>
+							<td class="hidden NADHOut">
+								2 NADH
+							</td>
+						</tr>
+					</tbody>
+				</table>
+				<div class="animation">
+					<div class="glucose hidden">
+						<div class="circle carbon"></div>
+						<div class="circle carbon"></div>
+						<div class="circle carbon"></div>
+						<div class="circle carbon"></div>
+						<div class="circle carbon"></div>
+						<div class="circle carbon"></div>	
+					</div>
+				</div>
+			</div>
+			<div class="section" id="section1">
+				<h1>Intermediate Step</h1>
+			</div>
+			<div class="section" id="section2">
+				<h1>Krebs Cycle</h1>
+			</div>
+			<div class="section" id="section3">
+				<h1>Electron Transport Chain</h1>
+			</div>
+		</div>
 	</body>
 </html>
